55f3be04ee08ad0087f176a1498edff36f0226d0
Hari <hariprasad.kunde@gmail.com>
2026-05-19 09:19:19 +0530

    made payload dynamic with ExternalInputs

--- src/test/java/basics/DynamicPayloadWithExternalInputs.java
+++ src/test/java/basics/DynamicPayloadWithExternalInputs.java
@@ -13,12 +13,12 @@ public class DynamicPayloadWithExternalInputs {
 	@Test
 	public void addBook() {
 		RestAssured.baseURI = "http://216.10.245.166";
-		String response = given().header("ContentType", "appplication/json").body(LibraryPayload.addBook())
+		String response = given().header("ContentType", "appplication/json").body(LibraryPayload.addBook("asdf", "1234"))
 		.when().post("/Library/Addbook.php")
 		.then().log().all().assertThat().statusCode(200).extract().response().asString();
 		
 		JsonPath js = new JsonPath(response);
 		String bookID = js.get("ID");
-		System.out.println("Book ID:" + bookID);
+		System.out.println("Book ID: " + bookID);
 	}
 }
--- src/test/java/payloads/LibraryPayload.java
+++ src/test/java/payloads/LibraryPayload.java
@@ -2,11 +2,11 @@ package payloads;
 
 public class LibraryPayload {
 
-	public static String addBook() {
+	public static String addBook(String isbn, String aisle) {
 		return "{\r\n"
 				+ "    \"name\": \"Learn Appium Automation with Java\",\r\n"
-				+ "    \"isbn\": \"bcdf\",\r\n"
-				+ "    \"aisle\": \"2255\",\r\n"
+				+ "    \"isbn\": \""+isbn+"\",\r\n"
+				+ "    \"aisle\": \""+aisle+"\",\r\n"
 				+ "    \"author\": \"Johnson\"\r\n"
 				+ "}";
 	}
