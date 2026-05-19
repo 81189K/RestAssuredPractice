617d45a4e003ee34b2b2f07581f1789b440ab908
Hari <hariprasad.kunde@gmail.com>
2026-05-19 09:13:44 +0530

    added Library POST API

---
+++ src/test/java/basics/DynamicPayloadWithExternalInputs.java
@@ -0,0 +1,24 @@
+package basics;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import payloads.LibraryPayload;
+
+import static io.restassured.RestAssured.*;
+
+public class DynamicPayloadWithExternalInputs {
+
+	@Test
+	public void addBook() {
+		RestAssured.baseURI = "http://216.10.245.166";
+		String response = given().header("ContentType", "appplication/json").body(LibraryPayload.addBook())
+		.when().post("/Library/Addbook.php")
+		.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		
+		JsonPath js = new JsonPath(response);
+		String bookID = js.get("ID");
+		System.out.println("Book ID:" + bookID);
+	}
+}
---
+++ src/test/java/payloads/LibraryPayload.java
@@ -0,0 +1,13 @@
+package payloads;
+
+public class LibraryPayload {
+
+	public static String addBook() {
+		return "{\r\n"
+				+ "    \"name\": \"Learn Appium Automation with Java\",\r\n"
+				+ "    \"isbn\": \"bcdf\",\r\n"
+				+ "    \"aisle\": \"2255\",\r\n"
+				+ "    \"author\": \"Johnson\"\r\n"
+				+ "}";
+	}
+}
