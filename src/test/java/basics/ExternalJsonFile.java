af78e47efd1465157a9101bd3fb34a6840cef88e
Hari <hariprasad.kunde@gmail.com>
2026-05-19 10:18:20 +0530

    json payload file as input

---
+++ src/test/java/basics/ExternalJsonFile.java
@@ -0,0 +1,43 @@
+package basics;
+
+import static io.restassured.RestAssured.given;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class ExternalJsonFile {
+	
+	@Test
+    public void externalJsonTest() throws Exception {
+
+        // Read external JSON file: [JSON > Bytes[] > String]
+        String requestBody = new String(
+                Files.readAllBytes(
+                        Paths.get("src/test/resources/payloads/addPlace.json")
+                )
+        );
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        given()
+                .log().all()
+                .header("Content-Type", "application/json")
+                .queryParam("key", "qaclick123")
+                .body(requestBody)
+
+        .when()
+                .post("maps/api/place/add/json")
+
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("scope", Matchers.equalTo("APP"));
+    }
+
+}
